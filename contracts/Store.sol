716e51bb8403a9b270f4d779838238d7caacc75d
Muskan Marwah <marwahmuskan330@gmail.com>
2022-11-01 17:51:33 +0530

    [Muskan] Completed Store.sol

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -3,9 +3,44 @@
 pragma solidity >0.8.0;
 
 contract Store {
-    address owner;
 
-    constructor (address _ownerAddress) {
-        owner = _ownerAddress;
+    // add events for product add and purchase
+    // add is_unavailable for Product
+    
+    address public storeOwner;
+    string public storeName;
+
+    struct Product {
+        string productUUID;
+        uint256 price;
+        string ipfsHash;
+    }
+
+    mapping (string => Product) products;
+
+    modifier onlyOwner {
+      require(msg.sender == storeOwner);
+      _;
+   }
+
+    constructor (address _ownerAddress, string memory _storeName) {
+        storeOwner = _ownerAddress;
+        storeName = _storeName;
+    }
+
+    function add_product(string memory _productUUID, uint256 _price, string memory _ipfsHash) public {
+        products[_ipfsHash] = Product(_productUUID, _price, _ipfsHash);
+    }
+
+    function product_by_ipfs_hash(string memory _ipfsHash) public view returns (string memory, uint256, string memory) {
+        Product memory product = products[_ipfsHash];
+        return (product.productUUID, product.price, product.ipfsHash);
+    }
+
+    function purchase_product_by_ipfs_hash(string memory _ipfsHash) payable public {
+        Product memory product = products[_ipfsHash];
+        require(msg.value >= product.price);
+        (bool sent, bytes memory data) = storeOwner.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
     }
 }
--- contracts/StoreCreator.sol
+++ contracts/StoreCreator.sol
@@ -12,7 +12,7 @@ contract StoreCreator {
     event NewStore (string indexed storeName, address indexed storeAddress, address indexed storeOwnerAddress);
     event StoreStatus (address indexed storeOwnerAddress, bool status);
 
-    address storeCreatorOwnerAddress;
+    address public storeCreatorOwnerAddress;
 
     struct StoreInfo {
         address storeAddress;
@@ -23,23 +23,32 @@ contract StoreCreator {
 
     mapping (address => StoreInfo) userStore;
 
+    modifier onlyOwner {
+      require(msg.sender == storeCreatorOwnerAddress);
+      _;
+   }
+
     constructor () {
         storeCreatorOwnerAddress = msg.sender;
     }
 
     function addStore (string memory _storeName) public {
-        Store newStore = new Store(msg.sender);
+        Store newStore = new Store(msg.sender, _storeName);
         address newStoreAddress = address(newStore);
         userStore[msg.sender] = StoreInfo(newStoreAddress, msg.sender, _storeName, true);
         emit NewStore(_storeName, newStoreAddress, msg.sender);
     }
 
     function pauseStore () public {
+        require(userStore[msg.sender].ownerAddress == msg.sender);
+        require(msg.sender == storeCreatorOwnerAddress);
         userStore[msg.sender].isActive = false;
         emit StoreStatus(msg.sender, false);
     }
 
     function resumeStore () public {
+        require(userStore[msg.sender].ownerAddress == msg.sender);
+        require(msg.sender == storeCreatorOwnerAddress);
         userStore[msg.sender].isActive = true;
         emit StoreStatus(msg.sender, true);
     }
